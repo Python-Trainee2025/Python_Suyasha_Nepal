dca92ce89a11dacb594978163487e907ae14be91
Suyasha Nepal <snepal@infics.com>
2025-10-27 12:37:47 +0545

    first commit

---
+++ basic.py
@@ -0,0 +1,16 @@
+help('keywords')
+
+"""
+Python is case sensitive
+Indentation is important
+Semi-colons are not required
+variables can't be keywords
+Title case for classes (class PersonName)
+lowercase for variables and functions
+uppercase= constant (by convention)
+no need to declare data types exclusively
+"""
+
+name= 'Ram'
+print(type(name))
+
